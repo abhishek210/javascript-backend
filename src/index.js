71102b0e82800595c174db24bd55364a29942937
Abhishek <abhishek.y@reward360.co>
2024-03-11 18:50:09 +0530

    utils folder discusion

--- src/index.js
+++ src/index.js
@@ -5,8 +5,28 @@ dotenv.config({
   path: "./env",
 });
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongo db connection failed !!", error);
+  });
 
+
+
+
+
+
+
+
+
+
+
+
+  
 // import express from "express";
 
 // const app = express();
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) =>
+      next(error)
+    );
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler = (func) => async (req, res, next) => {
+//   try {
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message,
+//     });
+//   }
+// };
